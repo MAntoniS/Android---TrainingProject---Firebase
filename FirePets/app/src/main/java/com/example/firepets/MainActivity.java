5719cabc26e98771556ae6996815a80e11084d97
MAntoniS <sobota.antoni@gmail.com>
2020-04-13 18:26:43 -0700

    StartingPoint

--- FirePets/app/src/main/java/com/example/firepets/MainActivity.java
+++ FirePets/app/src/main/java/com/example/firepets/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.firepets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signInButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = (Button)findViewById(R.id.sign_in);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CatalogActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpButton = (Button)findViewById(R.id.create_new_user);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateUserActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
